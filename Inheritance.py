652e0a4e62465d55a784376a692686280b9337a9
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-05-14 21:43:19 -0700

    created a parent class and children classes

---
+++ Inheritance.py
@@ -0,0 +1,13 @@
+
+class Students:
+	fname = 'Mikasa'
+	lname = 'Ackerman'
+	email = 'mikasaack@pdx.edu'
+
+class Grade(Students):
+	grade = 'Junior'
+	status = 'On track to graduate'
+
+class standing(Students):
+	gpa = 3.5
+	percentile = '90%'
