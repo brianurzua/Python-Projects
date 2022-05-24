b824fed31c6310920f9ea16c8dd04ae4b2e0913b
brianurzua <98988561+brianurzua@users.noreply.github.com>
2022-05-23 21:05:56 -0700

    phonebook project

---
+++ Phonebook/phonebook_main.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import tkinter as tk
+
+# Importing our other modules so we can access them.
+import phonebook_gui
+import phonebook_func
+
+# Frame is the tkinter frame class that our own class will inherit from
+class ParentWindow(Frame):
+	def __init__(self, master, *args, **kwargs):
+		Frame.__init__(self, master,*args, **kwargs)
+
+		# define our master frame configuration
+		self.master = master
+		self.master.minsize(500,300)
+		self.master.maxsize(500,300)
+
+		# This CenterWindow method will center our app on the user's screen
+		phonebook_func.center_window(self, 500, 300)
+		self.master.title("The Tkinter Phonebook Demo")
+		self.master.configure(bg="#F0F0F0")
+
+		""" This protocol method is the tkinter built-in method to catch if
+			the user clicks the upper corner, 'X' on Windows OS """
+		self.master.protocol("WM_DELETE_WINDOW", lambda: phonebook_func.ask_quit(self))
+		arg = self.master
+
+		""" load in the GUI widgets from a separate module, keepin your
+			code comparmentalized and clutter free """
+		phonebook_gui.load_gui(self)
+
+
+if __name__ == "__main__":
+	root = tk.Tk()
+	App = ParentWindow(root)
+	root.mainloop()
